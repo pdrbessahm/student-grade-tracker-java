32234640883e01ad36157c3db3b98deaf4bf87ac
pdrbessahm <bessapedro500@gmail.com>
2025-06-25 21:12:27 -0300

    first code version

---
+++ grade-tracker/src/App.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+
+
+        System.out.println("Hello, World!");
+
+        
+    }
+}
+
+
+
+/*
+ * student grade tracker FEATURES
+ * - 5 options menu (add student, add grades, view report, remove student, exit)
+ * - add student must have a name and a student id as input
+ * - add grade must have 3 options to input the users grade
+ * - the view report option will have name, id, grades, grades average and their status (passed or not)
+ * - add a feedback message after finish every single part
+ */
